ad807dad8c126f5527072d74a0600ea4aa173e06
PraVishal <blu.vishal@gmail.com>
2026-02-25 11:42:05 +0400

    First commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+/*const fetch = require("node-fetch");*/
+
+const app = express();
+
+app.use(express.json());
+
+// Allow your frontend origin
+app.use(cors({
+  origin: "http://127.0.0.1:5501"
+}));
+
+app.post("/api/register", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://boapi3.smartico.ai/api/register-aff",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req.body)
+      }
+    );
+
+    const data = await response.text();
+    res.status(response.status).send(data);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Proxy error" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Proxy running at http://localhost:3000");
+});
